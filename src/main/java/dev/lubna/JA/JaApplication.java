243eaefa0c91be69595688dd1fe7cd3f69f57394
lubie-max <shaikhls2421@gmail.com>
2024-11-30 16:06:59 +0530

    first comment

---
+++ src/main/java/dev/lubna/JA/JaApplication.java
@@ -0,0 +1,13 @@
+package dev.lubna.JA;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/dev/lubna/JA/JaApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.lubna.JA;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
